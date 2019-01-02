6c8860e6c71abc257cb6784b4821e2c5cfe31985
Vardhan Dugar <vadugar@microsoft.com>
2019-01-02 07:24:52 -0800

    Updates

---
+++ precache-manifest.f8064ea374c86a30243f7052a7499a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10256b2700850e285109",
+    "url": "/app/static/js/runtime~main.10256b27.js"
+  },
+  {
+    "revision": "e0acad3dafe6f767f3b3",
+    "url": "/app/static/js/main.e0acad3d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e0acad3dafe6f767f3b3",
+    "url": "/app/static/css/main.d72a9731.chunk.css"
+  },
+  {
+    "revision": "a3c084ab181ee838dc9a325a7cec3673",
+    "url": "/app/index.html"
+  }
+];
